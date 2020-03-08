494d55c8f63c91f87d1c27a65abd5860296b723a
David Emanuelsen <david.manuelsen@outlook.com>
2020-03-08 02:46:44 +0100

    tweaks to regform, still a mess

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
@@ -8,19 +8,147 @@ import SignupForm from './Components/SignupForm'
 
 
 function App() {
+
+  const roles = [
+    {
+        label: 'Tank',
+        icon: 'icon-warrior.gif',
+        role: 1
+    },        
+    {
+        label: 'Hunter',
+        icon: 'icon-hunter.gif',
+        role: 2
+    },        
+    {
+        label: 'Druid',
+        icon: 'icon-druid.gif',
+        role: 3
+    },        
+    {
+        label: 'Warrior',
+        icon: 'icon-warrior.gif',
+        role: 4
+    },        
+    {
+        label: 'Mage',
+        icon: 'icon-mage.gif',
+        role: 5
+    },        
+    {
+        label: 'Shaman',
+        icon: 'icon-shaman.gif',
+        role: 6
+    },        
+    {
+        label: 'Rogue',
+        icon: 'icon-rogue.gif',
+        role: 7
+    },        
+    {
+        label: 'Warlock',
+        icon: 'icon-warlock.gif',
+        role: 8
+    },        
+    {
+        label: 'Priest',
+        icon: 'icon-priest.gif',
+        role: 9
+    }
+];
+
+  const [attendants, setAttendants] = useState([
+    {
+        role: 1,
+        name: 'Pingo'
+    },
+    {
+        role: 2,
+        name: 'Cauw'
+    },
+    {
+        role: 3,
+        name: 'Test'
+    },
+    {
+        role: 4,
+        name: 'Hey'
+    },
+    {
+        role: 4,
+        name: 'SomethingRandom'
+    },
+    {
+        role: 5,
+        name: 'Pingo'
+    },
+    {
+        role: 6,
+        name: 'Pingo'
+    },
+    {
+        role: 6,
+        name: 'Pingo'
+    },
+    {
+        role: 6,
+        name: 'Pingo'
+    },
+    {
+        role: 7,
+        name: 'Pingo'
+    },
+    {
+        role: 8,
+        name: 'Pingo'
+    },
+    {
+        role: 9,
+        name: 'Pingo'
+    }
+  ]);
+
+
+  const roster = [
+    {
+      name: 'Cauw',
+      role: 5
+    },    
+    {
+      name: 'Pingo',
+      role: 1
+    },    
+    {
+      name: 'Dernaus',
+      role: 3
+    },    
+    {
+      name: 'Vincenth',
+      role: 9
+    },    
+    {
+      name: 'Fork',
+      role: 5
+    }
+  ]
+  
+  let addAttendant = (attendant) => {
+    setAttendants([...attendants, attendant])
+  };
+
   return (
     <Container>
       <Row>
         <Col>
-          <h1 style={{paddingBottom: 30, paddingTop: 30}}>Valkyrie Raid Signup</h1>
-            
-          <CurrentRoster />
+          <h1 style={{ paddingBottom: 30, paddingTop: 30 }}>Valkyrie Raid Signup</h1>
+
+          <CurrentRoster roles={roles} attendants={attendants} />
           <br />
-          <SignupForm />
-          
+          <SignupForm roles={roles} addAttendant={addAttendant} roster={roster} />
+
         </Col>
       </Row>
-  </Container>
+    </Container>
   );
 }
 
--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -1,65 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../bootstrap.min.css';
-import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button, Card, ListGroup } from 'react-bootstrap'
 
 
-function SignupForm() {
-    return (
-        <Card>
+function SignupForm(props) {
+    let { addAttendant, roster, roles } = props;
+    
+    let [charName, setCharName] = useState('');
+    let [filteredRoster, setFilteredRoster] = useState([...roster]);
+    let [player, setPlayer] = useState(null);
+    let [registered, setRegistered] = useState(false);
+
+    console.log(filteredRoster)
+    let onCharNameChange = (e) => {
+        const userInput = e.currentTarget.value;
+    
+        setFilteredRoster([...(roster.filter(
+          (player) => player.name.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+        ))]);
+        setCharName(e.currentTarget.value);
+    };
+
+    let suggestionList;
+    if (charName.length === 0) {
+        suggestionList = <></>;
+    } else {
+        suggestionList = <ListGroup>
+            {filteredRoster.map(p => <ListGroup.Item action onClick={() => setPlayer(p)}>
+                <img src={roles.filter(x => x.role === p.role)[0].icon}/>
+                {p.name}
+            </ListGroup.Item>)}
+        </ListGroup>
+    }
+
+    let register = (r) => {
+        setRegistered(true);
+        addAttendant(r);
+    };
+
+    let signupCard;
+    if (registered) {
+        signupCard = <Card>
             <Card.Header>
-                Wanna join?
+                Great success!
+            </Card.Header>
+            <Card.Body>
+                Your response is registered, thank you!
+            </Card.Body>
+        </Card>
+    } else {
+        signupCard = <Card>
+            <Card.Header>
+                Wanna join? 
             </Card.Header>
             <Card.Body>
                 <Form>
-                <Form.Group controlId="charName">
-                    <Form.Label>
-                        Character name
-                    </Form.Label>
-                    <Form.Control type="text" placeholder="Enter character name" />
-                    <Form.Text className="text-muted">
-                        Enter your character name here, be honest! cba adding accounts yet
-                    </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Anything else on your mind?</Form.Label>
-                    <Form.Control as="textarea" rows="3" />
-                </Form.Group>
-                <fieldset>
-                    <Form.Group>
-                        <Form.Label as="legend">
-                            Are you in?!
-                        </Form.Label>
-                        <Form.Check
-                            type="radio"
-                            label="I will be able to attend fo-sho"
-                            name="formHorizontalRadios"
-                            id="formHorizontalRadios1"
-                        />
-                        <Form.Check
-                            type="radio"
-                            label="I might be able to attend.."
-                            name="formHorizontalRadios"
-                            id="formHorizontalRadios2"
-                        />
-                        <Form.Check
-                            type="radio"
-                            label="I'll bench myself, but will be available if needed"
-                            name="formHorizontalRadios"
-                            id="formHorizontalRadios3"
-                        />
-                    </Form.Group>
-                </fieldset>
-               
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="I have paid Cauw an entry fee of 1g" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Lets go!
-                </Button>
-            </Form>
+                    { player ? 
+                        <>
+                            <Card.Title>Hi <img style={{height:25, width:25}} src={roles.filter(x => x.role === player.role)[0].icon}/>{player.name}!</Card.Title> 
+                            <fieldset>
+                                <Form.Group>
+                                    <Form.Label as="legend" >
+                                        Are you in?!
+                                    </Form.Label>
+                                    <Form.Check
+                                        type="radio"
+                                        label="I will be able to attend fo-sho"
+                                        name="formHorizontalRadios"
+                                        id="formHorizontalRadios1"
+                                    />
+                                    <Form.Check
+                                        type="radio"
+                                        label="I might be able to attend.."
+                                        name="formHorizontalRadios"
+                                        id="formHorizontalRadios2"
+                                    />
+                                    <Form.Check
+                                        type="radio"
+                                        label="I'll bench myself, but will be available if needed"
+                                        name="formHorizontalRadios"
+                                        id="formHorizontalRadios3"
+                                    />
+                                </Form.Group>
+                            </fieldset>
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Anything else on your mind?</Form.Label>
+                                <Form.Control as="textarea" rows="3" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="I have paid Cauw an entry fee of 1g" />
+                            </Form.Group>
+                            <Button variant="primary" onClick={() => register(player)}>
+                                Lets go!
+                            </Button>
+                        </>
+                        : 
+                        <Form.Group controlId="charName">
+                            <Form.Label>
+                                Who are you?
+                            </Form.Label>
+                            <Form.Control type="text" placeholder="Find yourself" 
+                                onChange={onCharNameChange}
+                                value={charName}
+                                autocomplete="off"/>
+                            {suggestionList}
+                        </Form.Group>
+                    }
+                </Form>
             </Card.Body>
-            
         </Card>
+    }
+
+
+    return (
+        <>
+            {signupCard}
+        </>
     );
 }
 
